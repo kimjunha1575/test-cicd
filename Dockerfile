46502acb1301bfb7b29c6a7ee2cdccca2e71bd2c
kimjunha1575 <kimjunha1575@gmail.com>
2024-02-27 15:37:22 +0900

    0.9.1

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM openjdk:17-jdk-alpine3.13
-COPY build/libs/app.jar /app/app.jar
-WORKDIR ./app
+COPY build/libs/app.jar /app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/org/example/cicd/VersionController.java
+++ src/main/java/org/example/cicd/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
         @GetMapping("/version")
         public String getVersion() {
-            return "0.9.0";
+            return "0.9.1";
         }
 }
